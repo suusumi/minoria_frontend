29f504f2e0fccffe3b579cd6d00048acbb3774c0
Sergey Groshev <wer113wer@yandex.ru>
2024-08-19 01:22:41 +0300

    [feat](header): create header example

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,41 @@
-// myFunction.ts
-export default function add(a: number, b: number): number {
-    return a + b;
-}
+import {AppBar, Box, Button, Container, Icon, Toolbar} from "@mui/material";
+
+const pages = ['О нас', 'Продукты', 'Тутуту'];
+
+const AppBarStyles =  {
+    background: "white",
+    shadows: '0'
+};
+
+export const Header = () => {
+    // TODO: add adaptive
+    // TODO: use theme (not sx)
+    return (
+            <AppBar sx={AppBarStyles}>
+                {/*TODO: set container sizes*/}
+
+                <Container>
+                    <Toolbar disableGutters>
+                        <Box>
+                            <Icon sx={{width:'100px', height:'100%'}}>
+                                <img src="/minoria_full_logo.svg" alt="minoria-logo" style={{display:'flex', width: '100%'}}/>
+                            </Icon>
+                        </Box>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                    </Toolbar>
+                </Container>
+
+            </AppBar>
+
+    );
+};
