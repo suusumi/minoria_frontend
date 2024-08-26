b4d5405b91cc8e0d82bc2de929c9528c12cfbb1d
Sergey Groshev <wer113wer@yandex.ru>
2024-08-27 00:40:40 +0300

    [feat](theme): localstorage to selected theme
    used redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,23 @@
-import {configureStore} from "@reduxjs/toolkit";
-import theme from "./theme/themeSlice";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 
-export default configureStore({
-    reducer: {
-        theme,
-    },
+import storage from "redux-persist/lib/storage";
+import themeReducer from "./theme/themeSlice.ts"
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    theme: themeReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store =  configureStore({
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
