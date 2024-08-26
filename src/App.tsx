9f82ad761cd2fc44e1ceab601dca798c2c137329
Sergey Groshev <wer113wer@yandex.ru>
2024-08-26 23:48:05 +0300

    [feat](theme): light/dark theme (used redux)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import './App.css'
 import {Header} from "./components/header/Header.tsx";
 // import {BrowserRouter, Routes} from "react-router-dom";
-import {RegistrationForm} from "./components/registration/RegistrationForm.tsx";
+
 import {QueryClient, QueryClientProvider} from "react-query";
+import {RegistrationForm2} from "./components/registration/RegistrationForm2.tsx";
+import {Box, FormControlLabel, FormGroup, Switch, ThemeProvider} from "@mui/material";
+import {darkTheme, lightTheme} from "./theme/theme.ts";
+import {useDispatch, useSelector} from "react-redux";
+import {toggleTheme} from "./redux/theme/themeSlice.ts";
 
 const queryClient = new QueryClient();
 
 function App() {
+    // Получить тему из стора
+    const theme = useSelector((state) => state.theme)
 
-  return (
-    <QueryClientProvider client={queryClient}>
-        <Header/>
-        <RegistrationForm/>
-        {/*<BrowserRouter>*/}
-        {/*    <Routes>*/}
+    // Диспатч для переключателя
+    const dispatch = useDispatch();
 
-        {/*    </Routes>*/}
-        {/*</BrowserRouter>*/}
-    </QueryClientProvider>
-  )
+    const ToggleSwitch = () => {
+        return (
+            <div
+                style={{
+
+                    top: "10px",
+                    right: "10px",
+                }}
+            >
+                <FormGroup>
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={theme.darkTheme}
+                                onChange={() => dispatch(toggleTheme())}
+                            />
+                        }
+                        label="Toggle Theme"
+                    />
+                </FormGroup>
+            </div>
+        );
+    };
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme.darkTheme ? darkTheme : lightTheme}>
+                <Header/>
+                <Box sx={{marginTop:'200px'}}>
+                    <ToggleSwitch />
+                </Box>
+                <RegistrationForm2/>
+                {/*<BrowserRouter>*/}
+                {/*    <Routes>*/}
+
+                {/*    </Routes>*/}
+                {/*</BrowserRouter>*/}
+            </ThemeProvider>
+        </QueryClientProvider>
+    )
 }
 
 export default App
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,15 +1,39 @@
 import {createTheme} from "@mui/material";
 import {purple, red} from "@mui/material/colors";
 
-const theme = createTheme({
-    status: {
-        danger: red[500],
-    },
-    palette: {
-        primary: {
-            main: purple[500],
-        }
-    }
-})
+// const theme = createTheme({
+//     status: {
+//         danger: red[500],
+//     },
+//     palette: {
+//         primary: {
+//             main: purple[500],
+//         }
+//     }
+// })
 
-export default theme;
+// export default theme;
+
+export const lightTheme = createTheme({
+    palette: {
+        mode: "light",
+        background: {
+            paper: "#f2f2f2",
+        },
+        text: {
+            primary: "#11111",
+        },
+    },
+});
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+        background: {
+            paper: "#222",
+        },
+        text: {
+            primary: "#fff",
+        },
+    },
+});
